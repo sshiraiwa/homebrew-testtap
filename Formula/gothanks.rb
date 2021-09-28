ff5acfd42bd9103ed72ab94676e6c323eb7beb65
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2021-09-28 09:21:58 -0400

    gothanks 0.3.0 (new formula)

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -1,6 +1,6 @@
 class Gothanks < Formula
   desc "Test"
-  homepage ""
+  homepage "https://github.com/psampaz/gothanks"  
   url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
