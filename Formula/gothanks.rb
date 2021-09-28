b7b72971d66d38054c091b06b074c42279e977a1
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2021-09-28 09:15:10 -0400

    gothanks 0.3.0 (new formula)

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -1,5 +1,5 @@
 class Gothanks < Formula
-  desc "GoThanks automatically stars Go's official repository and your go.mod github dependencies, providing a simple way  to say thanks to the maintainers of the modules you use and the contributors of Go itself."
+  desc "GoThanks automatically stars Go's official "
   homepage ""
   url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
