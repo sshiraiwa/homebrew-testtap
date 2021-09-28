804da0ee88b312e54443854a8478418e7b669173
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2021-09-28 09:10:29 -0400

    gothanks 0.3.0 (new formula)

---
+++ Formula/gothanks.rb
@@ -0,0 +1,19 @@
+class Gothanks < Formula
+  desc "GoThanks automatically stars Go's official repository and your go.mod github dependencies, providing a simple way  to say thanks to the maintainers of the modules you use and the contributors of Go itself."
+  homepage ""
+  url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
+  sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    ENV.delete "GITHUB_TOKEN"
+    assert_match "no Github token found", shell_output(bin/"gothanks", 255)    
+  end
+end
